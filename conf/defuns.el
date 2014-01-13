7cb9547d6e07eb06a6806e28b5a6b627e669fc1f
kinghom <bestkindy@gmail.com>
2014-01-13 10:33:35 +0800

    update submodules

--- conf/defuns.el
+++ conf/defuns.el
@@ -1682,3 +1682,18 @@ programming."
                :height 30
                :scroll-bar t
                :margin t)))
+
+
+(defun copy-line-or-region ()
+  "Copy current line, or current text selection."
+  (interactive)
+  (if (region-active-p)
+      (kill-ring-save (region-beginning) (region-end))
+    (kill-ring-save (line-beginning-position) (line-beginning-position 2))))
+
+(defun cut-line-or-region ()
+  "Cut the current line, or current text selection."
+  (interactive)
+  (if (region-active-p)
+      (kill-region (region-beginning) (region-end))
+    (kill-region (line-beginning-position) (line-beginning-position 2))))
