08066b57c890aeed3fc690e87efcf8969050c62e
kinghom <bestkindy@gmail.com>
2014-12-29 16:39:14 +0800

    update company-conf.el

--- conf/company-conf.el
+++ conf/company-conf.el
@@ -31,11 +31,13 @@
      (add-to-list 'company-backends 'company-files t)
      (add-to-list 'company-backends 'company-semantic t)
      ; (add-to-list 'company-backends 'company-cider)
+     ;; company-c-headers
      (add-to-list 'company-backends 'company-c-headers)
      ;; can't work with TRAMP
-     (setq company-backends (delete 'company-ropemacs company-backends))
-     (setq company-backends (delete 'company-capf company-backends))
-     (setq company-backends (delete 'company-clang company-backends))
+     (delete 'company-ropemacs company-backends)
+     (delete 'company-capf company-backends)
+     (delete 'company-clang company-backends)
+     (delete 'company-semantic company-backends)
      ;; I don't like the downcase word in company-dabbrev
      ;; for languages use camel case naming convention
      (setq company-dabbrev-downcase nil)
